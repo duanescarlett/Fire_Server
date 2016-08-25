c70cd9fab6ca83592fb12230479bf29f09dacce5
guitartool <duanescarlett@gmail.com>
2016-08-25 08:20:35 +0000

    Commit  25/08/16 3:20 am

--- src/ClientSession.java
+++ src/ClientSession.java
@@ -108,7 +108,11 @@ class ClientSession {
                 for(int i = 0; i < allUsers.size(); i++){ // Send user one by one to client from array list
                     output("UserList:" + allUsers.get(i).toString());
                     System.out.println("UserList:" + allUsers.get(i).toString());
-
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     allUsers.remove(i);
                 }
 
